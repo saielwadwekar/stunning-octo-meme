1695383150cf74ebbd28fac1a3ae7709e4bd2cc0
Saiel Wadwekar <punchh@Saiels-Mac-Book.local>
2022-09-13 16:50:48 +0530

    Tweets Controller

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,25 @@
+class TweetsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def delete
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
